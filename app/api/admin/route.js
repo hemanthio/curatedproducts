2d8927372b898a8219e954ad84377fd3e56c0863
hemanthio <121523327+hemanthio@users.noreply.github.com>
2025-01-03 14:59:28 +0530

    supabase url

--- app/api/admin/route.js
+++ app/api/admin/route.js
@@ -2,8 +2,13 @@ import { NextResponse } from "next/server";
 // import { supabase } from '/lib/supabase';
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseUrl = 'https://sinhuqhzbdfdhuquxnxs.supabase.co';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase URL and Key must be provided as environment variables.");
+}
+
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
