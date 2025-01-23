7202c5d2c595d97963259ed0175deb00bb52f483
hemanthio <121523327+hemanthio@users.noreply.github.com>
2025-01-23 13:49:35 +0530

    posthog setup

--- app/layout.js
+++ app/layout.js
@@ -4,10 +4,12 @@ import { GeistSans } from "geist/font/sans";
 import { useState } from "react";
 import Categories from "../components/Categories";
 import Navbar from "../components/Navbar";
+import { CSPostHogProvider } from "./providers";
 
 const metadata = {
   title: "CuratedProducts",
-  description: "Discover the Essentials from collection of curated websites for Your Next Big Idea",
+  description:
+    "Discover the Essentials from collection of curated websites for Your Next Big Idea",
 };
 
 export default function RootLayout({ children }) {
@@ -18,25 +20,28 @@ export default function RootLayout({ children }) {
       <head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        <link rel="icon" href="/favicon.ico" /> {/* Updated the icon path to favicon.ico */}
+        <link rel="icon" href="/favicon.ico" />{" "}
+        {/* Updated the icon path to favicon.ico */}
       </head>
       <body className={GeistSans.className}>
+      <CSPostHogProvider>
         <div className="min-h-screen bg-white">
           <Navbar toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
-          
+
           {/* Main Content */}
           <div className="flex mt-16">
-            <Categories 
-              isOpen={isSidebarOpen} 
-              onClose={() => setIsSidebarOpen(false)} 
+            <Categories
+              isOpen={isSidebarOpen}
+              onClose={() => setIsSidebarOpen(false)}
             />
-            
+
             {/* Content Area */}
             <main className="flex-1 transition-all duration-300 md:ml-64 p-6">
               {children}
             </main>
           </div>
         </div>
+        </CSPostHogProvider>
       </body>
     </html>
   );
